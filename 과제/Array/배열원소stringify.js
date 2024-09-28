be73ed8aaf12b3481f11a96b22b1fe79c3166a4d
AhYoungJo <doubletwosep@gmail.com>
2024-09-28 18:31:47 +0900

    배열의 원소를 stringify하기

--- 과제/Array/배열원소stringify.js
+++ 과제/Array/배열원소stringify.js
@@ -1,11 +1,13 @@
 // ex1) 배열의 각 원소를 String으로 변환하시오.
 const assert = require('assert');
 const arr = [1, 2, 3, true];
-const ret1 = arr.map(e => e.toString()); //<--
+// const ret1 = arr.map(e => e.toString()); //<--
+const ret1 = arr.map(String); //<--
 assert.deepStrictEqual(ret1, ['1', '2', '3', 'true']);
 // ex2) 다음과 같이 작동하는 classNames 함수를 작성하시오.
 const classNames = (...args) => {
-    return args.filter(v => v !== '').join(' '); //<--
+    // return args.filter(v => v !== '').join(' '); //<--
+    return args.filter(Boolean).join(' '); //<--
 };
 const ret2 = classNames('', 'a b c', 'd', '', 'e');
 assert.strictEqual(ret2, 'a b c d e');
