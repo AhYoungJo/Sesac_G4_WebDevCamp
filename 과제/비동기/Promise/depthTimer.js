dc637c54640767de2978fab9cfad60b77d3cba2e
AhYoungJo <doubletwosep@gmail.com>
2024-10-19 00:48:17 +0900

    promise로 리팩토링하기

---
+++ 과제/비동기/Promise/depthTimer.js
@@ -0,0 +1,24 @@
+const depthTimer = val =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(`depth${val}`, new Date());
+            val <= 3
+                ? resolve(val + 1)
+                : reject(new Error('Already 3-depth!!'));
+        }, 1000 * val);
+    });
+
+console.log('START!', new Date());
+
+depthTimer(1).then(depthTimer).then(depthTimer).catch(console.error);
+
+// setTimeout(function () {
+//     console.log('depth1', new Date());
+//     setTimeout(function () {
+//         console.log('depth2', new Date());
+//         setTimeout(function () {
+//             console.log('depth3', new Date());
+//             throw new Error('Already 3-depth!!');
+//         }, 3000);
+//     }, 2000);
+// }, 1000);
