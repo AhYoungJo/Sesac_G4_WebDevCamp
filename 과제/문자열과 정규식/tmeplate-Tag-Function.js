99c4f7ffa7152bff7ce517a2c74d5def3581b515
ahyoung <doubletwosep@gmail.com>
2024-10-18 12:58:55 +0900

    template tag function

--- 과제/문자열과 정규식/tmeplate-Tag-Function.js
+++ 과제/문자열과 정규식/tmeplate-Tag-Function.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
 
-const total = {price: 45000, vat: 4500};
+const total = { price: 45000, vat: 4500 };
 
-function fmt(txt, price) {
-    return String.raw(txt, price);
+function fmt([txt, unit], price) {
+    return `${txt}${price.toLocaleString().padStart(1)}${unit}`;
 }
 
-assert.strictEqual(fmt`주문합계: ${total.price}원`, '주문합계: 45000원');
-assert.strictEqual(fmt`세액합계: ${total.vat}원`, '세액합계: 4500원');
+assert.strictEqual(fmt`주문합계: ${total.price}원`, '주문합계: 45,000원');
+assert.strictEqual(fmt`세액합계: ${total.vat}냥`, '세액합계: 4,500냥');
