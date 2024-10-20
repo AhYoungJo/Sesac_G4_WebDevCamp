b9a3a7399875b78ae64f5636a2c4938bf6d4e598
AhYoungJo <doubletwosep@gmail.com>
2024-10-20 23:29:34 +0900

    promiseAll 직접 구현하기

--- 과제/비동기/Promise/promiseAll.js
+++ 과제/비동기/Promise/promiseAll.js
@@ -1,30 +1,7 @@
 const assert = require('assert');
-const {error} = require('console');
-
-const randTime = val =>
-    new Promise(resolve => {
-        setTimeout(() => {
-            resolve(val);
-        }, val);
-    });
+const randTime = require('./randTime');
 
 const vals = [1, 2, 3];
-
-// Promise.all([randTime(1), randTime(2), randTime(3)])
-//     .then(arr => {
-//         console.table(arr);
-//         assert.deepStrictEqual(arr, vals);
-//     })
-//     .catch(console.error);
-
-// Promise.all([randTime(11), Promise.reject('RRR'), randTime(33)])
-//     .then(array => {
-//         console.table(array);
-//         console.log('여긴 과연 호출될까?!');
-//     })
-//     .catch(error => {
-//         console.log('reject!!!!!!>>', error);
-//     });
 
 const promiseAll = arr => {
     const promiseArr = [];
