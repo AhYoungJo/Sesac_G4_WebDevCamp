27a60849444068e6933bf7254fa0269c0be354e8
ahyoung <doubletwosep@gmail.com>
2024-10-21 17:08:07 +0900

    resolve 발생 조건 더 명료하게 수정

--- 과제/비동기/Promise/promiseAll.js
+++ 과제/비동기/Promise/promiseAll.js
@@ -10,28 +10,36 @@ const promiseAll = arr => {
         // Promise.all(arr)
         //     .then(res => resolve(res))
         //     .catch(reject);
-        arr.forEach(promise =>
+
+        let length = arr.length;
+        arr.forEach((promise, idx) =>
             promise
                 .then(res => {
-                    promiseArr.push(res);
-                    if (promiseArr.length === arr.length) resolve(promiseArr);
+                    promiseArr[idx] = res;
+                    length--;
+                    if (length === 0) resolve(promiseArr);
                 })
-                .catch(err => reject({state: 'rejected', reason: err})),
+                .catch(err => reject({ state: 'rejected', reason: err })),
         );
+
     });
 };
 
+console.time('promiseAll');
 promiseAll([randTime(1), randTime(2), randTime(3)])
     .then(arr => {
         console.table(arr);
+        console.timeEnd('promiseAll');
         assert.deepStrictEqual(arr, vals);
     })
     .catch(console.error);
 
+console.time('promiseAll2');
 promiseAll([randTime(11), Promise.reject('RRR'), randTime(33)])
     .then(array => {
         console.log('여긴 과연 호출될까?!');
         console.table(array);
+        console.timeEnd('promiseAll2');
     })
     .catch(error => {
         console.log('reject!!!!!!>>', error);
