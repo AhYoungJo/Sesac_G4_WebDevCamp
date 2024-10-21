93756950891069faa886689ae6f87d20bf03157f
ahyoung <doubletwosep@gmail.com>
2024-10-21 17:06:40 +0900

    promiseAll async await version

--- 과제/비동기/Async_await/promiseAll.js
+++ 과제/비동기/Async_await/promiseAll.js
@@ -1,34 +1,152 @@
 const assert = require('assert');
-const randTime = require('../Promise/randTime.js');
+const randTime = value =>
+    new Promise(resolve => {
+        const rand_T = Math.random() * 1000;
+        setTimeout(() => {
+            resolve(value);
+        }, rand_T);
+    });
 const vals = [1, 2, 3];
 
-const promiseAll = arr => {
+//두 번째 테스트 코드 통과 한 코드
+//성호님
+const promiseAllAsync_성호 = async promises => {
     const promiseArr = [];
-    return new Promise(async (resolve, reject) => {
-        for (const promise of arr.values()) {
+    for (const promise of promises) {
+        promise.catch(err => err);
+    };
+    //rejected로 간주한 promise에 대한 처리가 없으면 에러가 발생
+    for (const idx in promises) {
+        const res = await promises[idx];
+        promiseArr[idx] = res;
+    }
+    return promiseArr;
+};
+
+// promiseAllAsync_성호([randTime(1), randTime(2), randTime(3)])
+//     .then(arr => {
+//         console.table(arr);
+//         assert.deepStrictEqual(arr, vals);
+//     })
+//     .catch(console.error);
+
+// promiseAllAsync_성호([randTime(11), Promise.reject('RRR'), randTime(33)])
+//     .then(array => {
+//         console.log('여긴 과연 호출될까?!');
+//     })
+//     .catch(error => {
+//         console.log('reject!!!!!!>>', error);
+//     });
+
+//----------------------------------------------------
+
+//기환님
+const promiseAllAsync_기환 = async (promises) => {
+    const results = [];
+    let cntToRun = promises.length;
+
+    if (promises.length === 0) {
+        return results;
+    }
+
+    return new Promise((resolve, reject) => {
+        promises.forEach(async (promise, index) => {
             try {
-                const res = await promise;
-                promiseArr.push(res);
-            } catch (err) {
-                reject({state: 'rejected', reason: err});
-                return;
+                const result = await promise;
+                results[index] = result;
+                cntToRun--;
+                if (cntToRun === 0) {
+                    resolve(results);
+                }
+            } catch (error) {
+                reject(error);
             }
-        }
-        resolve(promiseArr);
+        });
     });
 };
 
-promiseAll([randTime(1), randTime(2), randTime(3)])
-    .then(arr => {
-        console.table(arr);
-        assert.deepStrictEqual(arr, vals);
-    })
-    .catch(console.error);
+// promiseAllAsync_기환([randTime(1), randTime(2), randTime(3)])
+//     .then(arr => {
+//         console.table(arr);
+//         assert.deepStrictEqual(arr, vals);
+//     })
+//     .catch(console.error);
 
-promiseAll([randTime(11), Promise.reject('RRR'), randTime(33)])
-    .then(array => {
-        console.log('여긴 과연 호출될까?!');
-    })
-    .catch(error => {
-        console.log('reject!!!!!!>>', error);
-    });
+// promiseAllAsync_기환([randTime(11), Promise.reject('RRR'), randTime(33)])
+//     .then(array => {
+//         console.log('여긴 과연 호출될까?!');
+//     })
+//     .catch(error => {
+//         console.log('reject!!!!!!>>', error);
+//     });
+
+//----------------------------------------------------
+
+//두 번째 테스트 코드 통과 못한 코드
+//async/await은 try catch문, 나는 왜 안 될까?
+//아영
+const promiseAllAsync_아영 = async promises => {
+    const promiseArr = [];
+    for (const promise of promises) {
+        try {
+            const res = await promise;
+            promiseArr.push(res);
+        } catch (err) {
+            return { state: 'rejected', reason: err };
+        }
+    }
+    return promiseArr;
+};
+
+// promiseAllAsync_아영([randTime(1), randTime(2), randTime(3)])
+//     .then(arr => {
+//         console.table(arr);
+//         assert.deepStrictEqual(arr, vals);
+//     })
+//     .catch(console.error);
+
+// promiseAllAsync_아영([randTime(11), Promise.reject('RRR'), randTime(33)])
+//     .then(array => {
+//         console.log('여긴 과연 호출될까?!');
+//     })
+//     .catch(error => {
+//         console.log('reject!!!!!!>>', error);
+//     });
+
+
+//----------------------------------------------------
+
+//지연
+async function promiseAllAsync_지연(arr) {
+    const ret = []; //프로미스 결과배열
+    let cnt = 0;
+
+    if (arr.length === 0) {
+        return Promise.resolve([]);
+    }
+
+    //ver1
+    for (i = 0; i < arr.length; i++) {
+        try {
+            const p = await Promise.resolve(arr[i]); //프로미스결과 기다리기
+            ret[i] = p; //결과배열에 프로미스결과 넣기
+            cnt++;
+        } catch {
+            return Promise.reject(p);
+        }
+    }
+
+    if (cnt === arr.length) return Promise.resolve(ret); //마지막에 한번만 resolve
+}
+
+//지연님 테스트코드
+// (async () => {
+//     try {
+//         const arr = await promiseAllAsync_지연([randTime(1), randTime(2), randTime(3)]);
+//         console.table(arr);
+//         //assert.deepStrictEqual(arr, vals);
+//     } catch (err) {
+//         console.error("reject>>", err);
+//     }
+// })(); //익명 async로 묶으면 최상위 await사용가능함
+
