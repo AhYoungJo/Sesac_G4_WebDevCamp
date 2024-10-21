4f27e03f1f624589959d18543186bbc5b626fd0b
ahyoung <doubletwosep@gmail.com>
2024-10-21 17:08:50 +0900

    병렬 비동기 처리용 randTime 함수

--- 과제/비동기/Promise/randTime.js
+++ 과제/비동기/Promise/randTime.js
@@ -1,10 +1,22 @@
-console.time('randTime');
-const randTime = value =>
+// console.time('randTimes');
+const randTimes = value =>
     new Promise(resolve => setTimeout(resolve, Math.random * value, value));
 
-randTime(100).then(res => {
-    console.log(res);
-    console.timeEnd('randTime');
-});
+// randTimes(100).then(res => {
+//     console.log(res);
+//     // console.timeEnd('randTimes');
+// });
+
+const randTime = value =>
+    new Promise(resolve => {
+        const rand_T = Math.random() * 1000;
+        setTimeout(() => {
+            resolve(value);
+        }, rand_T);
+    });
+
+// randTime(100).then(res => {
+//     console.log(res);
+// });
 
 module.exports = randTime;
